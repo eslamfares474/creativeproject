ae9fc1ab5a691237e51389f7016d7559c6af0a34
eslamfares474 <eslamfares474@gmail.com>
2020-11-04 19:02:05 +0200

    first project

---
+++ js/plugins.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    //navbar toggler
+    $('.navbar-toggler').on('click',function(){
+        
+    $('nav .one').toggleClass('top');
+    $('nav .two').toggleClass('hidden');
+    $('nav .three').toggleClass('bottom');
+    })
+})
